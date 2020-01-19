e64a1151d74666f86d8dae105008d3a80a2b1a6d
tumaa <ramanfatuu@gmail.com>
2020-01-19 11:09:05 +0300

    add all file

---
+++ javascript/tuu.js
@@ -0,0 +1,10 @@
+
+  
+  var typed = new Typed('.moringa', {
+    strings: ["FATUMA ALI",
+    "Software Developer",
+        'In Web Applications'],
+    typeSpeed:60,
+    backSpeed:60,
+    loop:true
+  });
